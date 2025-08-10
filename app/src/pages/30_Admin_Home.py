99b5422fbfe4096c2362e5a295eeec42bdacfe38
abhithati <abhinavthati@gmail.com>
2025-08-10 10:34:18 -0400

    modified create member button

--- app/src/pages/30_Admin_Home.py
+++ app/src/pages/30_Admin_Home.py
@@ -13,7 +13,7 @@ SideBarLinks()
 st.title("System Admin Home Page")
 
 if st.button(
-    "Member Profile",
+    "Create Member",
     type="primary",
         use_container_width=True):
     st.switch_page("pages/005_Create_Member.py")
